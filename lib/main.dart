091c71f228d8696db2786c5834ae475eaf336f27
mirikang <wheatme.k@gmail.com>
2023-05-20 03:48:34 +0900

    feat - chat ui 적용

--- lib/main.dart
+++ lib/main.dart
@@ -2,69 +2,129 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
+void main() {
+  runApp(ChatApp());
 }
 
-class _MyAppState extends State<MyApp> {
-  String _generatedText = "";
+class ChatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ChatGPT English Study App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ChatScreen(),
+    );
+  }
+}
 
-  Future<void> generateText() async {
-    String prompt = "The quick brown fox jumps over the lazy dog";
-    String model = "text-davinci-002";
-    String apiKey = "<YOUR_API_KEY>";
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
 
-    var response = await http.post(
-      Uri.parse('https://api.openai.com/v1/engines/$model/completions'),
+class _ChatScreenState extends State<ChatScreen> {
+  late TextEditingController _inputController;
+  late List<ChatMessage> _messages;
+
+  @override
+  void initState() {
+    super.initState();
+    _inputController = TextEditingController();
+    _messages = [];
+  }
+
+  @override
+  void dispose() {
+    _inputController.dispose();
+    super.dispose();
+  }
+
+  void _sendMessage(String text) async {
+    setState(() {
+      _messages.add(ChatMessage(text: text, isUserMessage: true));
+    });
+
+    final response = await http.post(
+      Uri.parse('https://api.openai.com/v1/chat/completions'),
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer $apiKey',
+        'Authorization': 'Bearer YOUR_API_KEY',
       },
       body: jsonEncode({
-        'prompt': prompt,
-        'max_tokens': 100,
-        'temperature': 0.5,
-        'n': 1,
-        'stop': '.'
+        'messages': [
+          {'role': 'system', 'content': 'You are a helpful assistant.'},
+          {'role': 'user', 'content': text},
+        ],
       }),
     );
 
     if (response.statusCode == 200) {
-      var data = jsonDecode(response.body);
+      final jsonResponse = jsonDecode(response.body);
+      final completions = jsonResponse['choices'][0]['message']['content'];
       setState(() {
-        _generatedText = data['choices'][0]['text'];
-      });
-    } else {
-      setState(() {
-        _generatedText = "Error: ${response.reasonPhrase}";
+        _messages.add(ChatMessage(text: completions, isUserMessage: false));
       });
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('GPT-3 Text Generation'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: generateText,
-                child: Text('Generate Text'),
-              ),
-              SizedBox(height: 20),
-              Text(_generatedText),
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ChatGPT English Study App'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: _messages.length,
+              itemBuilder: (context, index) {
+                final message = _messages[index];
+                return ListTile(
+                  title: Text(
+                    message.text,
+                    textAlign: message.isUserMessage
+                        ? TextAlign.right
+                        : TextAlign.left,
+                  ),
+                );
+              },
+            ),
           ),
-        ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _inputController,
+                    decoration: InputDecoration(hintText: 'Enter your message'),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.send),
+                  onPressed: () {
+                    final text = _inputController.text.trim();
+                    if (text.isNotEmpty) {
+                      _sendMessage(text);
+                      _inputController.clear();
+                    }
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
+}
+
+class ChatMessage {
+  final String text;
+  final bool isUserMessage;
+
+  ChatMessage({required this.text, required this.isUserMessage});
 }
