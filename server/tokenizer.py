2e00da08c58bd15279b6b7756551a000606bceac
dinnfish <134122584+dinnfish@users.noreply.github.com>
2023-05-28 14:41:56 +0900

    Update tokenizer.py

--- server/tokenizer.py
+++ server/tokenizer.py
@@ -14,6 +14,19 @@ text = input("텍스트를 입력하세요: ")
 # 단어별 개수 세기
 counts = count_words(text)
 
-# 결과 출력
+# 결과 출력 및 어려움 수준 분류
+print("텍스트 내 단어 개수:")
 for word, count in counts.items():
     print(f"{word}: {count}")
+
+# 단어 종류 수에 따른 어려움 수준 분류
+unique_words = len(counts)
+if unique_words <= 5:
+    level = "Beginner"
+elif unique_words <= 10:
+    level = "Intermediate"
+else:
+    level = "Advanced"
+
+# 어려움 수준 출력
+print("텍스트의 어려움 수준:", level)
