c0a06dbe4eef8fbbc9f7fc5b9dca392ccdf71f0f
mirikang <wheatme.k@gmail.com>
2023-06-02 01:29:48 +0900

    speech - add listening/not listening

--- front/lib/screens/speech_screen.dart
+++ front/lib/screens/speech_screen.dart
@@ -1,7 +1,6 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
-import 'package:flat/widgets/speech_input_widget.dart';
-import 'package:flat/widgets/text_input_widget.dart';
 
 class SpeechScreen extends StatefulWidget {
   @override
@@ -11,6 +10,7 @@ class SpeechScreen extends StatefulWidget {
 class _SpeechScreenState extends State<SpeechScreen> {
   stt.SpeechToText _speechToText = stt.SpeechToText();
   String _recognizedText = '';
+  // List<String> _recognizedTextList = [];
   bool _isListening = false;
 
   @override
@@ -22,29 +22,60 @@ class _SpeechScreenState extends State<SpeechScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Speech Screen'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(_recognizedText),
-            // TextInputWidget(),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: _startListening,
-              child: Text('Start Listening'),
-            ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: _stopListening,
-              child: Text('Stop Listening'),
-            ),
-            SpeechInputWidget(),
-          ],
+        appBar: AppBar(
+          title: Text('Speech'),
         ),
-      ),
+        body: Stack(
+          children: [
+            Align(
+                alignment: Alignment.center,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(_recognizedText),
+                    SizedBox(height: 16),
+                    FloatingActionButton(
+                      onPressed: _startListening,
+                      child: Icon(Icons.mic),
+                    ),
+                    SizedBox(height: 16),
+                    FloatingActionButton(
+                      //ElevatedButton
+                      onPressed: _stopListening,
+                      child: Icon(Icons.stop),
+                    ),
+                    // SpeechInputWidget(),
+                  ],
+                )),
+            Positioned(top: 16, right: 16, child: _buildListeningStatus())
+          ],
+        ));
+  }
+
+  Widget _buildListeningStatus() {
+    Color backgroundColor = _isListening ? Colors.green : Colors.red;
+    String statusText = _isListening ? 'Listening...' : 'Not Listening';
+
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          width: 12,
+          height: 12,
+          decoration: BoxDecoration(
+            color: backgroundColor,
+            shape: BoxShape.circle,
+          ),
+        ),
+        SizedBox(width: 8),
+        Text(
+          statusText,
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
     );
   }
 
@@ -55,22 +86,31 @@ class _SpeechScreenState extends State<SpeechScreen> {
     if (available) {
       setState(() {
         _isListening = true;
+        _recognizedText = '';
       });
 
+      Timer(Duration(seconds: 5), () {
+        print('Timeout occurred');
+        _stopListening();
+      });
       _speechToText.listen(
         onResult: (result) {
           setState(() {
             _recognizedText = result.recognizedWords;
           });
         },
+        listenFor: Duration(seconds: 5),
+        pauseFor: Duration(seconds: 5),
       );
     }
   }
 
   void _stopListening() {
     _speechToText.stop();
+    print('stop');
     setState(() {
       _isListening = false;
     });
+    // todo api call
   }
 }
