d331a6128644e1a81feefe6cdd9a3de1f5a1f754
mirikang <wheatme.k@gmail.com>
2023-06-10 03:04:29 +0900

    settings 에 level, topic선택 추가

--- app/lib/screens/settings_screen.dart
+++ app/lib/screens/settings_screen.dart
@@ -1,4 +1,5 @@
 // 주제, 난이도 설정 화면
+import 'dart:convert';
 import 'package:flutter/material.dart';
 
 class SettingsScreen extends StatefulWidget {
@@ -7,18 +8,39 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
+  String selectedLevel = ''; // 선택된 Level을 저장할 변수
+  int codeValue = 0; // 코드값을 저장할 변수
+  List<String> selectedSubjects = [];
+
+  TextStyle titleStyle = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    color: Colors.black87,
+  );
+
+  List<Map<String, dynamic>> subjectList = [];
+
   @override
   void initState() {
     super.initState();
+    selectedSubjects = [];
+    loadSituationsData().then((data) {
+      setState(() {
+        subjectList = data;
+      });
+    });
+  }
+
+  Future<List<Map<String, dynamic>>> loadSituationsData() async {
+    final situationsData = await DefaultAssetBundle.of(context)
+        .loadString('assets/situations.json');
+    final situationsJson = json.decode(situationsData);
+
+    return List<Map<String, dynamic>>.from(situationsJson['topics']);
   }
 
   @override
   Widget build(BuildContext context) {
-    TextStyle titleStyle = TextStyle(
-      fontSize: 16,
-      fontWeight: FontWeight.bold,
-      color: Colors.black87,
-    );
     return Scaffold(
         appBar: AppBar(
           title: Text('Settings'),
@@ -28,19 +50,72 @@ class _SettingsScreenState extends State<SettingsScreen> {
                 padding: EdgeInsets.all(16.0),
                 child: Column(
                   children: <Widget>[
-                    Text('Topic & Situation', style: titleStyle),
-                    Expanded(
-                      child: Card(
-                        child: SizedBox.expand(),
-                      ),
+                    Text('Level', style: titleStyle),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Radio<String>(
+                          value: '1',
+                          groupValue: selectedLevel,
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              selectedLevel = newValue!;
+                              codeValue = 1;
+                            });
+                          },
+                        ),
+                        Text('Beginner'),
+                        Radio<String>(
+                          value: '2',
+                          groupValue: selectedLevel,
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              selectedLevel = newValue!;
+                              codeValue = 2;
+                            });
+                          },
+                        ),
+                        Text('Intermediate'),
+                        Radio<String>(
+                          value: '3',
+                          groupValue: selectedLevel,
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              selectedLevel = newValue!;
+                              codeValue = 3;
+                            });
+                          },
+                        ),
+                        Text('Advanced'),
+                      ],
                     ),
                     Divider(),
-                    Text('Level', style: titleStyle),
+                    Text('Topic & Situation', style: titleStyle),
                     Expanded(
-                      child: Card(
-                        child: SizedBox.expand(),
+                      child: ListView.builder(
+                        itemCount: subjectList.length,
+                        itemBuilder: (context, index) {
+                          final subject = subjectList[index];
+                          final subjectId = subject['id'];
+                          final subjectName = subject['subject'];
+
+                          return CheckboxListTile(
+                            title: Text(subjectName),
+                            value: selectedSubjects.contains(subjectId),
+                            onChanged: (bool? newValue) {
+                              setState(() {
+                                if (newValue!) {
+                                  selectedSubjects.add(subjectId);
+                                } else {
+                                  selectedSubjects.remove(subjectId);
+                                }
+                              });
+                            },
+                          );
+                        },
                       ),
                     ),
+                    SizedBox(height: 50),
                   ],
                 ))));
   }
